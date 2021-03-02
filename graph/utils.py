11c32689853afa97279e3019e502eb62fc65262f
alifta <af4166@mun.ca>
2021-03-01 23:28:08 -0330

    using vscode commiting some changes for the first time

--- graph/utils.py
+++ graph/utils.py
@@ -2,33 +2,21 @@
 # ------
 
 import os
-# import sys
+import json
+import pickle
+
 
 import numpy as np
+import pandas as pd
 
 import sqlite3
 
+import matplotlib.pyplot as plt
+from pylab import matplotlib, cm
+
+
 # Database
 # --------
-
-
-def db_row_count(db, table, output=False):
-    """
-    Count the number of entries in specified table of database
-    """
-    try:
-        con = sqlite3.connect(db)
-        cur = con.cursor()
-        cur.execute('SELECT COUNT(*) FROM {}'.format(table))
-        cur.close()
-    except sqlite3.Error as e:
-        print(e)
-    finally:
-        if (con):
-            con.close()
-    count = cur.fetchall()[0][0]
-    if output: print(f'{table} has {count} entries.')
-    return count
 
 
 def db_execute(db, query):
@@ -64,71 +52,194 @@ def db_execute_many(db, query, data):
             con.close()
 
 
-# System
-# ------
+def db_row_count(db, table, output=False):
+    """
+    Count the number of entries in specified table of database
+    """
+    try:
+        con = sqlite3.connect(db)
+        cur = con.cursor()
+        cur.execute('SELECT COUNT(*) FROM {}'.format(table))
+        cur.close()
+    except sqlite3.Error as e:
+        print(e)
+    finally:
+        if (con):
+            con.close()
+    count = cur.fetchall()[0][0]
+    if output: print(f'{table} has {count} entries.')
+    return count
 
 
-def dir_walk(path, ext='', save=False):
+# Files & Folders
+# ---------------
+
+
+def folder_walk(path, ext='', save=False):
     """
-    Walk through a directory, find all the file with specified extension
+    Iterate inside folder and find all files with specified extension
     """
-    # Set extension to a specific extension if needed
     f = []
     for root, dirs, files in os.walk(path):
         for file in files:
-            # relative path of file
+            # Relative path
             relative_path = os.path.join(root, file)
-            # extention (type of file)
+            # Extention or type of file
             ext_of_file = os.path.splitext(relative_path)[-1].lower()[1:]
-            # if extension is set and equal to what we want
+            # If extension is set and equal to desired type
             if ext != '' and ext_of_file == ext:
                 f.append(os.path.abspath(relative_path))
-            # if extension is not set add the file anyway
+            # If extension is not set, meaning we want all the files
             else:
                 f.append(os.path.abspath(relative_path))
     f.sort()
-    if save:
-        np.savetxt('files.csv', f, delimiter=',', fmt='%s')
+    if save: np.savetxt('files.csv', f, delimiter=',', fmt='%s')
     return f
 
 
-def line_count(filename):
+def file_line_count(filename):
     """
-    Count the number of lines in the input file
+    Count number of lines in file
     """
-    # count = 0
-    # for line in open(filename).xreadlines(  ): count += 1
-    # return count
-    # OR
     return len(open(filename).readlines())
 
 
-# --- Colors ---
+# Colors
+# ------
 
 
 def colors_create(number_of_colors=1, color_map='Wistia', output=False):
     """
-    create a series of colors from the selected spectrum, e.g., Wistia [cold to hot]
+    create a list of colors from the selected spectrum e.g. Wistia, cold or hot
     """
-    color_list = []
+    colors = []
     cmap = cm.get_cmap(color_map, number_of_colors)
     for i in range(cmap.N):
-        # will return rgba, we take only first 3 so we get rgb
+        # Return rgba, but only first 3 is needed, not alpha
         rgb = cmap(i)[:3]
-        # print(matplotlib.colors.rgb2hex(rgb))
-        color_list.append(matplotlib.colors.rgb2hex(rgb))
+        colors.append(matplotlib.colors.rgb2hex(rgb))
     if output:
-        for i in range(len(color_list)):
-            plt.scatter(i, 1, c=color_list[i], s=20)
-    return color_list
+        for i,color in enumerate(colors):
+            plt.scatter(i, 1, c=color, s=20)
+    return colors
 
 
-# --- Data Structure ---
+# Structure
+# ---------
+
+def list_save(
+    input_list,
+    file_name='list',
+    file_postname='',
+    file_extension='csv',
+    delimiter=',',
+    replace=True,
+):
+    """
+    Method for saving list
+    """
+    if len(postname) > 0:
+        file_name = file_name + '_' + file_postname
+    file_path = os.path.join(OUTPUT_PATH, file_name + '.' + file_extension)
+    if os.path.exists(file_input):
+        print('file already exist.')
+        if replace:
+            print('overwriting ... ')
+            np.savetxt(file_path, input_list, delimiter=delimiter, fmt='%s')
+        else:
+            print('saving ... ')
+            file_path = os.path.join(
+                OUTPUT_PATH, file_name + '_new.' + file_extension
+            )
+            np.savetxt(file_path, input_list, delimiter=delimiter, fmt='%s')
+        print('done!')
+    else:
+        print('saving ... ')
+        np.savetxt(file_path, input_list, delimiter=delimiter, fmt='%s')
+        print('done!')
+
+
+def list_intersection(lst1, lst2):
+    """
+    Intersection of two list
+    In other words, all common elements of two lists
+    """
+    temp = set(lst2)
+    return [value for value in lst1 if value in temp]
+
+
+
+def dict_save(data, file='data', method='n', sort=False):
+    """
+    Save input dictionary using different method:
+        n: numpy (npy) -> good for saving the data type
+        c: pandas (csv) -> good for looking at data after saving and sorting
+        j: json -> also good for looking at data (simple key type) and sorting
+        p: pickle -> should be fastest, good for simple data type
+    """
+    # NPY
+    if method == 'n':
+        filename = file + '.npy'
+        np.save(filename, data)
+    # CSV
+    elif method == 'c':
+        filename = file + '.csv'
+        if not sort:
+            pd.DataFrame.from_dict(data, orient='index'
+                                   ).to_csv(filename, header=False)
+        else:
+            pd.DataFrame.from_dict(data, orient='index').sort_index(
+                axis=0
+            ).to_csv(filename, header=False)
+    # JSON
+    elif method == 'j':
+        filename = file + '.json'
+        with open(filename, 'w') as fp:
+            if not sort:
+                json.dump(data, fp)
+            else:
+                json.dump(data, fp, sort_keys=True, indent=4)
+    # Pickle
+    elif method == 'p':
+        filename = file + '.p'
+        with open(filename, 'wb') as fp:
+            pickle.dump(data, fp, protocol=pickle.HIGHEST_PROTOCOL)
+
+
+def dict_load(file='data', method='n'):
+    """
+    Load input file into a dictionary using different method:
+        n: numpy (npy)
+        c: pandas (csv)
+        j: json
+        p: pickle
+    """
+    data = {}
+    # npy
+    if method == 'n':
+        filename = file + '.npy'
+        data = np.load(filename, allow_pickle='True').item()
+    # csv
+    elif method == 'c':
+        filename = file + '.csv'
+        data = pd.read_csv(filename, header=None,
+                           index_col=0).T.to_dict('records')[0]
+    # json
+    elif method == 'j':
+        filename = file + '.json'
+        with open(filename, 'r') as fp:
+            data = json.load(fp)
+    # pickle
+    elif method == 'p':
+        filename = file + '.p'
+        with open(filename, 'rb') as fp:
+            data = pickle.load(fp)
+    return data
 
 
 def dict_add(dictionary, key, value):
     """
-    Add a key:value to dictionary if key does not already exist
+    Add {KEY:VALUE} to dict if key does not already exist
     """
     if key not in dictionary:
         dictionary[key] = value
@@ -136,10 +247,10 @@ def dict_add(dictionary, key, value):
 
 def dict_lookup(dictionary, key):
     """
-    Search the given KEY in dictionary ...
-    found -> return its value (could be an index assign to that value)
-    not found -> add the key and return its value (which is a new index)
-    useful for creating hash table of KEY->INDEX
+    Search KEY in dict
+    If found -> return key's value i.e index
+    If not found -> add key and return its value which is a new index
+    Useful for creating hash table {KEY:INDEX}
     """
     value = 0
     if key not in dictionary:
@@ -189,22 +300,7 @@ def label_amend(label_list, input_label, end=True):
     return label_list
 
 
-def list_intersection(lst1, lst2, version=4):
-    """
-    Intersection of two list or all common elements of two lists
-    """
-    if version == 1:
-        return [value for value in lst1 if value in lst2]
-    elif version == 2:
-        return list(set(lst1) & set(lst2))
-    elif version == 3:
-        if len(lst1) > len(lst2):
-            return set(lst1).intersection(lst2)
-        else:
-            return set(lst2).intersection(lst1)
-    elif version == 4:  # O(n)
-        temp = set(lst2)
-        return [value for value in lst1 if value in temp]
+
 
 
 def rank(x, return_rank=False):
@@ -239,7 +335,7 @@ def rank(x, return_rank=False):
     return list(ranked.index)
 
 
-# Test for rank
+# Test rank
 assert rank(
     {
         0: 2,
@@ -260,7 +356,7 @@ assert rank(
 
 def breakdown(lst, num):
     """
-    Breakdown a list into chunks of sub-list with size of n
+    Breakdown a list into chunks of sublist of size N
     """
     # pprint(list(range(len(lst))))
     # pprint(lst)
@@ -289,7 +385,8 @@ def breakdown(lst, num):
     return bd
 
 
-# --- Save & load ---
+# Figure
+# ------
 
 
 def fig_save(fig_id, tight_layout=True, fig_extension='png', resolution=300):
@@ -303,115 +400,8 @@ def fig_save(fig_id, tight_layout=True, fig_extension='png', resolution=300):
     plt.savefig(path, format=fig_extension, dpi=resolution)
 
 
-def dict_save(data, file='data', method='n', sort=False):
-    """
-    Save input dictionary using different method:
-        n: numpy (npy) -> good for saving the data type
-        c: pandas (csv) -> good for looking at data after saving and sorting
-        j: json -> also good for looking at data (simple key type) and sorting
-        p: pickle -> should be fastest, good for simple data type
-    """
-    # npy
-    if method == 'n':
-        filename = file + '.npy'
-        np.save(filename, data)
-    # csv
-    elif method == 'c':
-        filename = file + '.csv'
-        if not sort:
-            pd.DataFrame.from_dict(data, orient='index'
-                                   ).to_csv(filename, header=False)
-        else:
-            pd.DataFrame.from_dict(data, orient='index').sort_index(
-                axis=0
-            ).to_csv(filename, header=False)
-    # json
-    elif method == 'j':
-        filename = file + '.json'
-        with open(filename, 'w') as fp:
-            if not sort:
-                json.dump(data, fp)
-            else:
-                json.dump(data, fp, sort_keys=True, indent=4)
-    # pickle
-    elif method == 'p':
-        filename = file + '.p'
-        with open(filename, 'wb') as fp:
-            pickle.dump(data, fp, protocol=pickle.HIGHEST_PROTOCOL)
-
-
-def dict_load(file='data', method='n'):
-    """
-    Load input file into a dictionary using different method:
-        n: numpy (npy)
-        c: pandas (csv)
-        j: json
-        p: pickle
-    """
-    data = {}
-    # npy
-    if method == 'n':
-        filename = file + '.npy'
-        data = np.load(filename, allow_pickle='True').item()
-    # csv
-    elif method == 'c':
-        filename = file + '.csv'
-        data = pd.read_csv(filename, header=None,
-                           index_col=0).T.to_dict('records')[0]
-    # json
-    elif method == 'j':
-        filename = file + '.json'
-        with open(filename, 'r') as fp:
-            data = json.load(fp)
-    # pickle
-    elif method == 'p':
-        filename = file + '.p'
-        with open(filename, 'rb') as fp:
-            data = pickle.load(fp)
-    return data
-
-
-def list_save(
-    input_list,
-    file_name='list',
-    file_postname='',
-    file_extension='csv',
-    delimiter=',',
-    replace=True,
-):
-    """
-    Method for saving list
-    """
-    if len(postname) > 0:
-        file_name = file_name + '_' + file_postname
-    file_path = os.path.join(OUTPUT_PATH, file_name + '.' + file_extension)
-    if os.path.exists(file_input):
-        print('file already exist.')
-        if replace:
-            print('overwriting ... ')
-            np.savetxt(file_path, input_list, delimiter=delimiter, fmt='%s')
-        else:
-            print('saving ... ')
-            file_path = os.path.join(
-                OUTPUT_PATH, file_name + '_new.' + file_extension
-            )
-            np.savetxt(file_path, input_list, delimiter=delimiter, fmt='%s')
-        print('done!')
-    else:
-        print('saving ... ')
-        np.savetxt(file_path, input_list, delimiter=delimiter, fmt='%s')
-        print('done!')
-
-
-# --- Linear Algebra ---
-
-# def array_top_n(arr, top_N=1):
-#     """
-#     find top 'N' values in 2d numpy array
-#     """
-#     idx = np.argpartition(arr, arr.size - top_N, axis=None)[-top_N:][::-1]
-#     result = np.column_stack(np.unravel_index(idx, arr.shape))
-#     return [(e[0], e[1]) for e in result]
+# Linear Algebra
+# --------------
 
 
 def top_n(arr, top_N=1, index=True):
@@ -430,6 +420,11 @@ def array_top_n(arr, top_N=1):
     """
     find top 'N' values in 2d numpy array
     """
+    # (1)
+    # idx = np.argpartition(arr, arr.size - top_N, axis=None)[-top_N:][::-1]
+    # result = np.column_stack(np.unravel_index(idx, arr.shape))
+    # return [(e[0], e[1]) for e in result]
+    # (2)
     idx = (-arr).argsort(axis=None, kind='mergesort')
     # idx = (-arr).argsort(axis=None, kind='mergesort')[:top_N]
     result = np.vstack(np.unravel_index(idx, arr.shape)).T
@@ -445,23 +440,3 @@ def matrix_print(M, out_int=True):
             print(*element.astype(int))
         else:
             print(*element)
-
-
-# --- FOLDERS ---
-
-# --- Common Folders ---
-# ROOT_PATH = '.'
-ROOT_PATH = os.getcwd()
-DATA_PATH = os.path.join(ROOT_PATH, 'data')
-os.makedirs(DATA_PATH, exist_ok=True)
-IMAGE_PATH = os.path.join(ROOT_PATH, 'image')
-os.makedirs(IMAGE_PATH, exist_ok=True)
-# --- Project Specific Folders ---
-DB_PATH = os.path.join(ROOT_PATH, 'db')
-os.makedirs(DB_PATH, exist_ok=True)
-FILE_PATH = os.path.join(ROOT_PATH, 'file')
-os.makedirs(DB_PATH, exist_ok=True)
-NET_PATH = os.path.join(ROOT_PATH, 'network')
-os.makedirs(NET_PATH, exist_ok=True)
-HITS_PATH = os.path.join(ROOT_PATH, 'hits')
-os.makedirs(HITS_PATH, exist_ok=True)
