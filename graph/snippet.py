c151c10920bd746c56c90234ad7428ab1bcaf867
alifta <af4166@mun.ca>
2021-03-02 15:05:42 -0330

    finished utils and snippet codes

--- graph/snippet.py
+++ graph/snippet.py
@@ -1,8 +1,17 @@
 import numpy as np
 
+import matplotlib.pyplot as plt
+from numpy.core.fromnumeric import trace
+
 # List
 # ----
 
 # Save list to CSV file
 x = np.arange(0.0, 5.0, 1.0)
 np.savetxt('test.csv', x, delimiter=',', fmt='%s')
+
+# Figure
+# ------
+plt.tight_layout()
+plt.savefig(fname='figure.pdf', dpi=300, transparent=True,bbox_inches='tight')
+
