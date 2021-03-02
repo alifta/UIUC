bf7811e009e5b6bba11b891354343e0e764581c0
alifta <af4166@mun.ca>
2021-03-02 01:18:24 -0330

    add edit file paths method and yapf style

--- graph/__init__.py
+++ graph/__init__.py
@@ -1,23 +1,36 @@
 import os
 
-# Create the following folders in the project
+# Create a list of folders in the project path
+# Their name can be used throughout project
 
 # Package folder
 PACK_PATH = os.getcwd()
 
 # Root folder of project
-ROOT_PATH = os.getcwd()
+ROOT_PATH = os.path.dirname(PACK_PATH)
 
+# Data folder
 DATA_PATH = os.path.join(ROOT_PATH, 'data')
 os.makedirs(DATA_PATH, exist_ok=True)
-IMAGE_PATH = os.path.join(ROOT_PATH, 'image')
-os.makedirs(IMAGE_PATH, exist_ok=True)
-# --- Project Specific Folders ---
+
+print(DATA_PATH)
+
+# Database folder
 DB_PATH = os.path.join(ROOT_PATH, 'db')
 os.makedirs(DB_PATH, exist_ok=True)
+
+# Image folder
+IMAGE_PATH = os.path.join(ROOT_PATH, 'image')
+os.makedirs(IMAGE_PATH, exist_ok=True)
+
+# Extra files folder
 FILE_PATH = os.path.join(ROOT_PATH, 'file')
 os.makedirs(DB_PATH, exist_ok=True)
+
+# Graph folder
 NET_PATH = os.path.join(ROOT_PATH, 'network')
 os.makedirs(NET_PATH, exist_ok=True)
+
+# HITS score folder
 HITS_PATH = os.path.join(ROOT_PATH, 'hits')
 os.makedirs(HITS_PATH, exist_ok=True)
